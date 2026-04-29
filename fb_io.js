bd4462f2519e7d6f7a9c1dd2f4248a1ef9f20bc7
23501ap <23501ap@hvhs.school.nz>
2026-04-29 23:00:00 +0000

    Data

--- fb_io.js
+++ fb_io.js
@@ -6,34 +6,4 @@
  **                                                          **
  **************************************************************
  **************************************************************/
- function helloWorld(){
-  console.log("Running helloWorld()")
-  firebase.database().ref('/').set(
-    {
-      message: 'Kia ora te ao'
-    }
-  )
-}
-
- function GoodBye(){
-  console.log("GoodBye()")
-  firebase.database().ref('/').set(
-    {
-      message: 'ka kite āno'
-    }
-  )
-}
-
-function simpleRead(){
-console.log("Reading message");
-firebase.database().ref('/').child('message').once('value', displayRead);
-console.log("Leaving simpleRead")
-}
-
-firebase.database().ref('/message').once('value',DO_THIS )
-function DO_THIS(snapshot){console.log(snapshot.val());
-}
-function displayRead(snapshot){
-    console.log("Running displayRead(), the message is:" + snapshot.val())
-    HTML_OUTPUT.innerHTML = snapshot.val();
-}
+ 
--- script.js
+++ script.js
@@ -18,3 +18,39 @@ const HTML_OUTPUT = document.getElementById("databaseOutput");
 // This means it replaces the whole database with message:Hello World
 /**************************************************************/
 
+function helloWorld(){
+  console.log("Running helloWorld()")
+  firebase.database().ref('/').set(
+    {
+      message: 'Kia ora te ao'
+    }
+  )
+}
+
+ function GoodBye(){
+  console.log("GoodBye()")
+  firebase.database().ref('/').set(
+    {
+      message: 'ka kite āno'
+    }
+  )
+}
+
+function simpleRead(){
+console.log("Reading message");
+firebase.database().ref('/message').child('message').once('value', displayRead);
+console.log("Leaving simpleRead")
+}
+
+firebase.database().ref('/message').once('value',Users )
+function User(snapshot){console.log(snapshot.val());
+}
+function displayRead(snapshot){
+    console.log("Running displayRead), the message is:" + snapshot.val())
+    HTML_OUTPUT.innerHTML = snapshot.val();
+}
+
+function simpleRead(){
+
+    
+}
