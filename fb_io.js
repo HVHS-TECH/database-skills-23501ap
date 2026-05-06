848129097471b434a9a861317734a5081d7ea047
23501ap <23501ap@hvhs.school.nz>
2026-05-06 23:02:19 +0000

    gsf

--- fb_io.js
+++ fb_io.js
@@ -1,21 +1,88 @@
-/**************************************************************
- **************************************************************
- **                                                          **
- ** fb_io.js is where you will put common firebase functions **
- ** used throughout your code.                               **
- **                                                          **
- **************************************************************
- **************************************************************/
- firebase.database().ref('/').set(
-{
-    game1: {
-        users:{
+function readListener() {
+console.log("Setting up listener for me sage");
+firebase.database().ref('/message').on('value', snapshot.val() );
+
+}
+
+
+firebase.database().ref('users/Dhruv').set({
+    age: 67,
+    feet: 2,
+    hair
+})
+ firebase.database().ref('/game1').set(
+
+    { users:{
             Dhruv: 99999,
             Jack: 10000,
-            Toby: 9,
+            Micheal:"3.141",
+            Sasha:0.5,
             Yug: 987654321,
         }
     }
-}
 
  );
+
+ firebase.database().ref('/game2').set(
+
+{
+    
+        users:{
+            Dhruv:13,
+            Jack:14,
+            Mikaela:7,
+            Sasha:3,
+            Yug:12,
+        }
+    }
+ )
+
+
+firebase.database().ref('/game1/users/Jenna/').set(123456789);
+
+let user = "toby";
+let score = "0";
+firebase.database().ref('/game1/users/'+user).set(
+score
+
+);
+
+function updateHighscoreTable() {
+console.log("Updating")
+firebase.database().ref('/game1/users').update({
+Xavier: 67
+
+})
+
+}
+
+for (i = 0; i <names.lenght;i++){
+    let key = names[i];
+    console.log("Score "+i+" is for "+ key +". "+highScore[key] + "points." )
+}
+
+highscoreTable = {
+game1: {
+users: {
+Dhruv: 99999,
+Jack: 10000,
+Michael: "3.141",
+Sasha: 0.5,
+Yug: 987654321
+
+},
+game2: {
+users: {
+Dhruv: 13,
+Jack: 14,
+Mikaela: 7,
+Sasha: 3,
+Yug: 12,
+
+}
+
+}
+
+}
+}
+firebase.database().ref('/').set(highscoreTable)
--- script.js
+++ script.js
@@ -36,10 +36,6 @@ function helloWorld(){
   )
 }
 
-function Read(){
-console.log()
-
-}
 
 
 
