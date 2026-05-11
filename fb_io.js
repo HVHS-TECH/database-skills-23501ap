77bc5e1674a592c876c4790d7db4604190094bd9
23501ap <23501ap@hvhs.school.nz>
2026-05-11 01:29:53 +0000

    Buttons

--- fb_io.js
+++ fb_io.js
@@ -1,23 +1,6 @@
-firebase.database( ).ref( '/message' ).once( 'value', 'snapshot.val')
-function snapshot(snapshot){
-    console.log(snapshot.val());
-}
 
 
 
 
 
 
-Function simpleRead() function.
-function simpleRead() {
-console.log("Reading message");
-firebase.database().ref('/message').once('value', display);
-console.log("Leaving simpleRead")
-
-dd a display() function.
-
-function display(snapshot) {
-console.log("Running display(), the message is: " + snapshot.val())
-HTML_OUTPUT. innerHTML = snapshot.val();
-
-}
--- script.js
+++ script.js
@@ -18,8 +18,8 @@ const HTML_OUTPUT = document.getElementById("databaseOutput");
 // This means it replaces the whole database with message:Hello World
 /**************************************************************/
 
-function helloWorld(){
-  console.log("Running helloWorld()")
+function helloWorld() {
+  console.log("helloWorld")
   firebase.database().ref('/').set(
     {
       message: 'Kia ora te ao'
@@ -27,7 +27,7 @@ function helloWorld(){
   )
 }
 
- function GoodBye(){
+function GoodBye() {
   console.log("GoodBye()")
   firebase.database().ref('/').set(
     {
@@ -36,60 +36,57 @@ function helloWorld(){
   )
 }
 
-function readListener() {
-console.log("Setting up listener for me sage");
-firebase.database().ref('/message').on('value', snapshot.val() );
-
-}
-
- firebase.database().ref('/').set(
-{
+function Users1() {
+  console.log("Reading Users");
+firebase.database().ref('/').set(
+  {
     game1: {
-     users:{
-            Dhruv: 99999,
-            Jack: 10000,
-            Micheal:"3.141",
-            Sasha:0.5,
-            Yug: 987654321,
-        }
+      users: {
+        Dhruv: 99999,
+        Jack: 10000,
+        Micheal: "3.141",
+        Sasha: 0.5,
+        Yug: 987654321,
+      }
     }
-}
+  }
 );
-
-
- firebase.database().ref('/').set(
-
-{
-    game2: {
-        users:{
-            Dhruv:13,
-            Jack:14,
-            Mikaela:7,
-            Sasha:3,
-            Yug:12,
-        }
-    }
-}
- )
-
-
 firebase.database().ref('/game1/users/Jenna').set(123456789)
-
 let user = "toby";
 let score = "0";
-firebase.database().ref('/game1/users/'+user).set(
-score
+firebase.database().ref('/game1/users/' + user).set(
+  score
 
 );
-
-function Updatescore() {
-console.log("Updating")
-firebase.database().ref('/game1/users').update({
-Xavier: 67
-})
 }
 
-function Highscore(){
+function Users2(){
+firebase.database().ref('/').set(
+
+  {
+    game2: {
+      users: {
+        Dhruv: 13,
+        Jack: 14,
+        Mikaela: 7,
+        Sasha: 3,
+        Yug: 12,
+      }
+    }
+  }
+)
+}
+
+
+
+function Updatescore() {
+  console.log("Updating")
+  firebase.database().ref('/game1/users').update({
+    Xavier: 67
+  })
+}
+
+function Highscore() {
   console.log("Highscore")
   firebase.database().ref('/game1/users')
 }
@@ -112,24 +109,32 @@ function Highscore(){
 
 
 
-function display(snapshot){
+
+
+
+
+
+
+
+
+function display(snapshot) {
   var dbData = snapshot.val();
-  if (dbData ==null)
+  if (dbData == null)
     console.log(dbData)
 }
 
-function fb_readError(error){
+function fb_readError(error) {
   console.log("There was an error reading the message");
   console.error(error);
 }
 
-function fb_readlistener(){
-console.log("Read Listener");
-firebase.database().ref('/message').on('value',fb_logDatabaseRead)
+function fb_readlistener() {
+  console.log("Read Listener");
+  firebase.database().ref('/message').on('value', fb_logDatabaseRead)
 }
 
 
-function display(snapshot){
-console.log("Running display(), the message is:" + snapshot.val())
-HTML_OUTPUT.innerHTML = snapshot.val();
+function display(snapshot) {
+  console.log("Running display(), the message is:" + snapshot.val())
+  HTML_OUTPUT.innerHTML = snapshot.val();
 }
